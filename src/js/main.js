0886b1569e7011749dceadf72010df2d3cea0c3f
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2017-10-21 15:41:13 +0200

    Added logic for loader

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,22 @@ import Animate from './Animate';
     'use strict';
 
     window.addEventListener('load', () => {
+
         // Declare and instantiate animation object
         const anim = new Animate();
 
-        // Start the animation
-        anim.start();
+        // Fade out loader
+        const loader = document.getElementsByClassName('loader')[0];
+        loader.style.opacity = 0;
+
+        // Wait for fade out to complete and run after half a second
+        setTimeout(() => {
+
+            // Remove loader from DOM
+            loader.parentNode.removeChild(loader);
+
+            // Start animation
+            anim.start();
+        }, 500);
     });
 }
