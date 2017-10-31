f27cc13df5979160fc618e5ad30e7941e1a9fc18
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2017-10-31 15:53:34 +0100

    Replaced Map with an object to make a few more browser compatible such as IE10

--- src/js/Animate.js
+++ src/js/Animate.js
@@ -5,39 +5,39 @@ import { TweenLite, TimelineLite } from 'gsap';
  */
 export default class Animate {
     constructor() {
-        this.elements = new Map();
+        this.elements = {};
         this.retrieve();
         this.init();
     }
 
     /**
-     * Retrieves all needed DOM elements and stores them in a Map
+     * Retrieves all needed = doM elements and stores them in a Map
      */
     retrieve() {
         const titles = document.getElementsByClassName('title');
-        this.elements.set('left', document.querySelector('.block--left'));
-        this.elements.set('image', document.querySelector('.block__bg-img'));
-        this.elements.set('ctaWrapper', document.querySelector('.cta'));
-        this.elements.set('cta', document.querySelector('.cta a'));
-        this.elements.set('title', titles[0]);
-        this.elements.set('socials', document.querySelectorAll('.social__link'));
-        this.elements.set('sub', titles[1]);
-        this.elements.set('right', document.querySelector('.block--right'));
-        this.elements.set('content', document.getElementsByClassName('content'));
-        this.elements.set('body', document.querySelector('body'));
+        this.elements.left = document.querySelector('.block--left');
+        this.elements.image = document.querySelector('.block__bg-img');
+        this.elements.ctaWrapper = document.querySelector('.cta');
+        this.elements.cta = document.querySelector('.cta a');
+        this.elements.title = titles[0];
+        this.elements.socials = document.querySelectorAll('.social__link');
+        this.elements.sub =  titles[1];
+        this.elements.right = document.querySelector('.block--right');
+        this.elements.content = document.getElementsByClassName('content');
+        this.elements.body = document.querySelector('body');
     }
 
     /**
      * Positions the elements to their initial states
      */
     init() {
-        TweenLite.set(this.elements.get('left'), { xPercent: -100 });
-        TweenLite.set(this.elements.get('image'), { xPercent: 90 });
-        TweenLite.set(this.elements.get('cta'), { x: (this.elements.get('cta').offsetWidth * 2) });
-        TweenLite.set(this.elements.get('title'), { xPercent: -100, autoAlpha: 0 });
-        TweenLite.set(this.elements.get('sub'), { xPercent: -100, autoAlpha: 0 });
-        TweenLite.set(this.elements.get('socials'), { autoAlpha: 0 });
-        TweenLite.set(this.elements.get('content'), { y: 5, autoAlpha: 0 });
+        TweenLite.set(this.elements.left, { xPercent: -100 });
+        TweenLite.set(this.elements.image, { xPercent: 90 });
+        TweenLite.set(this.elements.cta, { x: (this.elements.cta.offsetWidth * 2) });
+        TweenLite.set(this.elements.title, { xPercent: -100, autoAlpha: 0 });
+        TweenLite.set(this.elements.sub, { xPercent: -100, autoAlpha: 0 });
+        TweenLite.set(this.elements.socials, { autoAlpha: 0 });
+        TweenLite.set(this.elements.content, { y: 5, autoAlpha: 0 });
     }
 
     /**
@@ -46,22 +46,22 @@ export default class Animate {
     start() {
         const timeline = new TimelineLite();
         timeline
-            .to(this.elements.get('left'), 0.1, { autoAlpha: 1 })
-            .to(this.elements.get('left'), 1.2, { xPercent: 0, ease: Power4.easeInOut })
-            .to(this.elements.get('image'), 0, {autoAlpha: 1 }, "-= 1.2")
-            .to(this.elements.get('image'), 1.2, { autoAlpha: 1, xPercent: 0, ease: Power4.easeInOut }, "-= 1.2")
-            .to(this.elements.get('title'), 0.7, { xPercent: 0, autoAlpha: 1, ease: Power4.easeInOut, delay: 0.5 }, "-= 1.2")
-            .to(this.elements.get('sub'), 0.7, { xPercent: 0, autoAlpha: 1, ease: Power4.easeInOut, delay: 0.55 }, "-= 1.15")
-            .to(this.elements.get('cta'), 0.7, { x: 0, ease: Power4.easeInOut, autoAlpha: 1, delay: 0.55, onCompleteScope: this.elements.get('cta'), onComplete: () => { this.end(); } }, "-= 1.1")
-            .staggerTo(this.elements.get('socials'), 0.3, { autoAlpha: 1 }, 0.05, "-= 0.1")
-            .staggerTo(this.elements.get('content'), 0.35, { y: 0, autoAlpha: 1, ease: Sine.easeOut }, 0.1, "-= 0.05");
+            .to(this.elements.left, 0.1, { autoAlpha: 1 })
+            .to(this.elements.left, 1.2, { xPercent: 0, ease: Power4.easeInOut })
+            .to(this.elements.image, 0, {autoAlpha: 1 }, "-= 1.2")
+            .to(this.elements.image, 1.2, { autoAlpha: 1, xPercent: 0, ease: Power4.easeInOut }, "-= 1.2")
+            .to(this.elements.title, 0.7, { xPercent: 0, autoAlpha: 1, ease: Power4.easeInOut, delay: 0.5 }, "-= 1.2")
+            .to(this.elements.sub, 0.7, { xPercent: 0, autoAlpha: 1, ease: Power4.easeInOut, delay: 0.55 }, "-= 1.15")
+            .to(this.elements.cta, 0.7, { x: 0, ease: Power4.easeInOut, autoAlpha: 1, delay: 0.55, onCompleteScope: this.elements.cta, onComplete: () => { this.end(); } }, "-= 1.1")
+            .staggerTo(this.elements.socials, 0.3, { autoAlpha: 1 }, 0.05, "-= 0.1")
+            .staggerTo(this.elements.content, 0.35, { y: 0, autoAlpha: 1, ease: Sine.easeOut }, 0.1, "-= 0.05");
     }
 
     /**
      * Deals with the final state
      */
     end() {
-        TweenLite.set(this.elements.get('ctaWrapper'), { css: { overflow: 'visible' } });
-        TweenLite.set(this.elements.get('body'), { css: { overflowY: 'visible' } });
+        TweenLite.set(this.elements.ctaWrapper, { css: { overflow: 'visible' } });
+        TweenLite.set(this.elements.body, { css: { overflowY: 'visible' } });
     }
 }
